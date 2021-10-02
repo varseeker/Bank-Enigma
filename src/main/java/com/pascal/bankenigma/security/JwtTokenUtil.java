971a293563ddcadf6fc8c57a7226b4523df98ea8
Ahsan <ibadahsan601@gmail.com>
2021-10-02 19:37:43 +0700

    [AHSAN][Add] Adding JwtTokenUtil.java for Security with jwt

---
+++ src/main/java/com/pascal/bankenigma/security/JwtTokenUtil.java
@@ -0,0 +1,46 @@
+package com.pascal.bankenigma.security;
+
+import com.pascal.bankenigma.service.UserDetailServiceDbImpl;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.w3c.dom.UserDataHandler;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtTokenUtil {
+
+    @Value("${secret}")
+    public String secret;
+
+    @Autowired
+    UserDetailServiceDbImpl userDetailServiceDb;
+
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userName", userDetails.getUsername());
+        claims.put("role", userDetails.getAuthorities());
+
+        String token = Jwts.builder()
+                .setClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+(5*60*1000)))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+        return token;
+    }
+
+    public UserDetails parseToken(String token){
+        Jws<Claims> jwsClaims = Jwts.parserBuilder().setSigningKey(secret).build().parseClaimsJws(token);
+        String userName = jwsClaims.getBody().getSubject();
+        return userDetailServiceDb.loadUserByUsername(userName);
+    }
+}
