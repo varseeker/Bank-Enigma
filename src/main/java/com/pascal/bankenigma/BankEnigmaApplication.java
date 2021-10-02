a636c8399e379114724fe4163c34c7a271e69fba
Ahsan <ibadahsan601@gmail.com>
2021-10-02 14:27:46 +0700

    [AHSAN][INIT] Initialization project for Enigma Bank

---
+++ src/main/java/com/pascal/bankenigma/BankEnigmaApplication.java
@@ -0,0 +1,13 @@
+package com.pascal.bankenigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankEnigmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankEnigmaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pascal/bankenigma/BankEnigmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pascal.bankenigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEnigmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
