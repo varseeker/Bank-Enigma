164af0913d4322d5dd3640c56a751f4294696fcc
Ahsan <ibadahsan601@gmail.com>
2021-10-02 19:40:35 +0700

    [AHSAN][Add] Adding and modified Main Application with security in BasicAuthenticationAdapter

--- src/main/java/com/pascal/bankenigma/BankEnigmaApplication.java
+++ src/main/java/com/pascal/bankenigma/BankEnigmaApplication.java
@@ -2,6 +2,7 @@ package com.pascal.bankenigma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
 public class BankEnigmaApplication {
